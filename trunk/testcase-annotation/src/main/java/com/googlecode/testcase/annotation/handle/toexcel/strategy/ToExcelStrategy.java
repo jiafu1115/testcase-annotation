c8f53214ec2095fea8dd09f313ac79ec6d636f91
fujian1115@gmail.com <fujian1115@gmail.com@7914cdbb-8740-3bcf-ec8a-25d6ff1f59a0>
2013-08-01 00:00:51 +0000

    support excel 2007

--- trunk/testcase-annotation/src/main/java/com/googlecode/testcase/annotation/handle/toexcel/strategy/ToExcelStrategy.java
+++ trunk/testcase-annotation/src/main/java/com/googlecode/testcase/annotation/handle/toexcel/strategy/ToExcelStrategy.java
@@ -3,6 +3,6 @@ package com.googlecode.testcase.annotation.handle.toexcel.strategy;
 import com.googlecode.testcase.annotation.wrapper.TestCaseWrapper;
 
 public interface ToExcelStrategy {
-	void add(TestCaseWrapper testCaseWrapper);
+	public void add(TestCaseWrapper testCaseWrapper);
 	public void generateExcelFile();
 }
