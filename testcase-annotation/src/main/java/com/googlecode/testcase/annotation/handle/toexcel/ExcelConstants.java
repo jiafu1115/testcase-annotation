ea126f725abfae1cfee1d5791c94a5dc21af2870
fujian1115 <fujian1115@gmail.com>
2013-08-03 23:28:17 +0000

    the default support excel type from xls to xlsx due to xls can't support some style settting.

--- testcase-annotation/src/main/java/com/googlecode/testcase/annotation/handle/toexcel/ExcelConstants.java
+++ testcase-annotation/src/main/java/com/googlecode/testcase/annotation/handle/toexcel/ExcelConstants.java
@@ -4,7 +4,7 @@ public class ExcelConstants {
 
 	public static final String DEFAULT_SHEET_NAME_WHEN_MORE_SHEET_EXIST = "others";
 	public static final String DEFAULT_SHEET_NAME_WHEN_ONLY_DEFAULT_MODULE_EXIST = "Test Cases";
-	public static final ExcelType DEFAULT_EXCEL_TYPE = ExcelType.XLS;
+	public static final ExcelType DEFAULT_EXCEL_TYPE = ExcelType.XLSX;
 	public static final String DEFAULT_EXCEL_NAME_FORMAT = "TestCase_%s%s";
 
 	private ExcelConstants(){
