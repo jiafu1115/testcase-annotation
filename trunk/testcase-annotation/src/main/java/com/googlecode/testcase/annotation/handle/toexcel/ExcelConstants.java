f482a81774d1e11c7934bd881b2c50b5350f9566
fujian1115@gmail.com <fujian1115@gmail.com@7914cdbb-8740-3bcf-ec8a-25d6ff1f59a0>
2013-08-10 02:51:25 +0000

    change the defined sheet name when only one default module existed.

--- trunk/testcase-annotation/src/main/java/com/googlecode/testcase/annotation/handle/toexcel/ExcelConstants.java
+++ trunk/testcase-annotation/src/main/java/com/googlecode/testcase/annotation/handle/toexcel/ExcelConstants.java
@@ -3,7 +3,7 @@ package com.googlecode.testcase.annotation.handle.toexcel;
 public class ExcelConstants {
 
 	public static final String DEFAULT_SHEET_NAME_WHEN_MORE_SHEET_EXIST = "others";
-	public static final String DEFAULT_SHEET_NAME_WHEN_ONLY_DEFAULT_MODULE_EXIST = "Test Cases";
+	public static final String DEFAULT_SHEET_NAME_WHEN_ONLY_DEFAULT_MODULE_EXIST = "Unnamed Module";
 	public static final ExcelType DEFAULT_EXCEL_TYPE = ExcelType.XLSX;
 	public static final String DEFAULT_EXCEL_NAME_FORMAT = "TestCase_%s%s";
 	public static final String FILE_SEPARATOR= System.getProperty("file.separator");
