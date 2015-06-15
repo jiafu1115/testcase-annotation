e326cafcb40d86ee816c53f0d84fd6c35b0a8a60
fu.jian <fujian1115@gmail.com>
2015-06-15 14:53:52 +0800

    Update OptionsConstants.java

--- testcase-annotation/src/main/java/com/googlecode/testcase/annotation/OptionsConstants.java
+++ testcase-annotation/src/main/java/com/googlecode/testcase/annotation/OptionsConstants.java
@@ -2,9 +2,9 @@ package com.googlecode.testcase.annotation;
 
 public class OptionsConstants {
 
-	public static final String EXCEL_TYPE_OPTION = "excelType";
-	public static final String FILE_NAME_OPTION = "file";
-	public static final String FOLDER_PATH_OPTION = "folder";
+	public static final String EXCEL_TYPE_OPTION = "TestCaseExcelType";
+	public static final String FILE_NAME_OPTION = "TestCaseFile";
+	public static final String FOLDER_PATH_OPTION = "TestCaseFolder";
  	public static final String FORCE_CREATE_FILE_OPTION = "forceCreateFile";
  
 	private OptionsConstants(){
